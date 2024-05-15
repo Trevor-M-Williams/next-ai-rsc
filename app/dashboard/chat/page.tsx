764ca12d40d0550a60b281f0558f2064dab726af
Trevor Williams <tmw7991@gmail.com>
2024-05-15 16:45:37 -0600

    feat: replace chat page with nacd demo

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -1,50 +1,8 @@
-"use client";
-
-import { useUIState, useActions } from "ai/rsc";
-import { UserMessage } from "@/components/chat/message";
-
-import { type AI } from "@/actions/chat/chat";
-import { ChatScrollAnchor } from "@/lib/hooks/chat-scroll-anchor";
-import { ChatList } from "@/components/chat/chat-list";
-import { EmptyScreen } from "@/components/chat/empty-screen";
-import { ChatInput } from "@/components/chat/chat-input";
-
-export default function ChatPage() {
-  const [messages, setMessages] = useUIState<typeof AI>();
-  const { submitUserMessage } = useActions<typeof AI>();
-
+export default async function ChatPage() {
   return (
-    <div className=" h-full overflow-auto">
-      <div className="pb-[200px] pt-4 md:pt-10">
-        {messages.length ? (
-          <>
-            <ChatList messages={messages} />
-          </>
-        ) : (
-          <EmptyScreen
-            submitMessage={async (message) => {
-              setMessages((currentMessages) => [
-                ...currentMessages,
-                {
-                  id: Date.now(),
-                  display: <UserMessage>{message}</UserMessage>,
-                },
-              ]);
-
-              const responseMessage = await submitUserMessage(message);
-              setMessages((currentMessages) => [
-                ...currentMessages,
-                responseMessage,
-              ]);
-            }}
-          />
-        )}
-        <ChatScrollAnchor trackVisibility={true} />
-      </div>
-      <ChatInput
-        setMessages={setMessages}
-        submitUserMessage={submitUserMessage}
-      />
-    </div>
+    <iframe
+      src="https://nacd.luminatedenver.dev"
+      className="w-full h-full"
+    ></iframe>
   );
 }
--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-console.log("DATABASE_URL", process.env.DATABASE_URL);
 const sql = neon(process.env.DATABASE_URL as string) as any;
 
 export const db = drizzle(sql, {
